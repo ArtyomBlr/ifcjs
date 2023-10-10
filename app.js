8de788bc62933a3fde598ec0255a1589a9043720
ArtyomBlr <artem.zborovsky2@gmail.com>
2023-10-10 22:24:52 +0300

    add file loading + shadowdropp

--- app.js
+++ app.js
@@ -1,89 +1,20 @@
-import {
-  AmbientLight,
-  AxesHelper,
-  DirectionalLight,
-  GridHelper,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-} from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { IFCLoader } from "web-ifc-three";
 
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
 
-//Creates the Three.js scene
-const scene = new Scene();
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
 
-//Object to store the size of the viewport
-const size = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+// Create grid and axes
 
-//Creates the camera (point of view of the user)
-const camera = new PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 15;
-camera.position.y = 13;
-camera.position.x = 8;
+viewer.grid.setGrid(40);
+viewer.axes.setAxes(20);
 
-//Creates the lights of the scene
-const lightColor = 0xffffff;
+async function loadIfc(url) {
+  const model = await viewer.IFC.loadIfcUrl(url);
 
-const ambientLight = new AmbientLight(lightColor, 0.5);
-scene.add(ambientLight);
+  await viewer.shadowDropper.renderShadow(model.modelID);
+  viewer.context.renderer.postProduction.active = true;
+}
 
-const directionalLight = new DirectionalLight(lightColor, 2);
-directionalLight.position.set(0, 10, 0);
-scene.add(directionalLight);
-
-//Sets up the renderer, fetching the canvas of the HTML
-const threeCanvas = document.getElementById("three-canvas");
-const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-//Creates grids and axes in the scene
-const grid = new GridHelper(50, 30);
-scene.add(grid);
-
-const axes = new AxesHelper();
-axes.material.depthTest = false;
-axes.renderOrder = 1;
-scene.add(axes);
-
-//Creates the orbit controls (to navigate the scene)
-const controls = new OrbitControls(camera, threeCanvas);
-controls.enableDamping = true;
-controls.target.set(-2, 0, 0);
-
-//Animation loop
-const animate = () => {
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-};
-
-animate();
-
-//Adjust the viewport to the size of the browser
-window.addEventListener("resize", () => {
-  (size.width = window.innerWidth), (size.height = window.innerHeight);
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(size.width, size.height);
-});
-
-// IFC loader
-
-const input = document.getElementById('file-input');
-const ifcLoader = new IFCLoader()
-
-input.addEventListener(
-  'change',
-  async (event) => {
-    const ifcURL = URL.createObjectURL(event.target.files[0]);
-    const model = await ifcLoader.loadAsync(ifcURL);
-    scene.add(model);
-  },
-  false
-);
+loadIfc('./assets/file.ifc');
